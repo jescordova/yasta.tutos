d957974c628bd09012f2a4a7df722a6e8dd58163
Jesus Cordova <jescordova@hotmail.com>
2025-11-18 23:25:35 +0100

    Installation Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,18 @@ import tailwindcss from '@tailwindcss/vite';
 import expressiveCode from "astro-expressive-code";
 import react from '@astrojs/react';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [
-    react(),
-    expressiveCode({
-      themes: ["dark-plus"],
-      defaultProps: {
-        wrap: true
-      }
-    })
-  ]
+  integrations: [react(), expressiveCode({
+    themes: ["dark-plus"],
+    defaultProps: {
+      wrap: true
+    }
+  }), svelte()]
 });
